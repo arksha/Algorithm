429fb8eefe6fb97a51f2f3cef40db807100f0ecb
arksha <tingg@usc.edu>
2016-02-13 20:57:40 -0800

    brute force 3 fors loop,follow up with 2pointers

--- 259. 3Sum Smaller.java
+++ 259. 3Sum Smaller.java
@@ -1,4 +1,4 @@
-259. 3Sum Smaller.java
+//259. 3Sum Smaller.java
 
 // Given an array of n integers nums and a target, find the number of index triplets i, j, k with 0 <= i < j < k < n that satisfy the condition nums[i] + nums[j] + nums[k] < target.
 
