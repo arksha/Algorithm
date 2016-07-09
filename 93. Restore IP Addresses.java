7a66e8450c59f59d77dfb6bf7cbdc54098cf1a11
arksha <tingg@usc.edu>
2016-07-09 09:54:45 -0700

    add new, need proceed

--- 93. Restore IP Addresses.java
+++ 93. Restore IP Addresses.java
@@ -1,12 +1,13 @@
-93. Restore IP Addresses
+// 93. Restore IP Addresses
 
-Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+// Given a string containing only digits, restore it by returning all possible valid IP address combinations.
 
-For example:
-Given "25525511135",
+// For example:
+// Given "25525511135",
 
-return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
-
+// return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+// --------------------------------------------------------------------------------------------------------------
+// dfs,  
 public class Solution {
     public List<String> restoreIpAddresses(String s) {
         List<String> res = new ArrayList<>();
@@ -23,6 +24,7 @@ public class Solution {
             if(s.length()<i) continue;
             int val = Integer.parseInt(s.substring(0,i));
             if(val>255||i!=String.valueOf(val).length()) continue;
+            /*in the case 010 the parseInt will return len=2 where val=10, but i=3, skip this.*/
             dfs(res,list+s.substring(0,i) + ".", p+1,s.substring(i));
         }
     }
