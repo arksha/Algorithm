9f238c782a1c3a8a7e7e2f49e6cd52644fdf5f7f
arksha <tingg@usc.edu>
2016-08-06 14:35:36 -0700

    add naive method with two BS

--- 74. Search a 2D Matrix.java
+++ 74. Search a 2D Matrix.java
@@ -4,7 +4,72 @@
 
 // Integers in each row are sorted from left to right.
 // The first integer of each row is greater than the last integer of the previous row.
+
+
 //----------------------------------------------------------------------------------------------------------------
+1.do twice binary search, once for finding row, once for column
+2.NB: because of (l+1<h), need to consider last two row, and have consider the row of h instead of l 
+    because if target > l maybe also >h, in this case target will be in row h. 
+3. can reuse l and h variable
+public class Solution {
+    /**
+     * @param matrix, a list of lists of integers
+     * @param target, an integer
+     * @return a boolean, indicate whether matrix contains target
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        // write your code here
+        if(matrix == null || matrix.length == 0){
+            return false;
+        }
+        if(matrix[0] == null || matrix[0].length == 0){
+            return false;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        
+        int l = 0, h = row-1;
+        while(l + 1 < h){
+            int rowm = l + (h - l) / 2;
+            if (matrix[rowm][0] == target){
+                return true;
+            }else if(matrix[rowm][0] < target){
+                l = rowm;
+            }else {
+                h = rowm;
+            }
+        }
+        if(matrix[h][0] <= target){
+            row = h;
+        }else if(matrix[l][0] <= target){
+            row = l;
+        } else {
+            return false;
+        }
+        
+        l = 0;
+        h = col-1;
+        while(l + 1 < h){
+            int colm = l + (h - l) / 2;
+            if(matrix[row][colm] == target){
+                return true;
+            }else if(matrix[row][colm] < target){
+                l = colm;
+            }else {
+                h = colm;
+            }
+        }
+        if(matrix[row][l] == target){
+            return true;
+        }else if(matrix[row][h] == target){
+            return true;
+        }
+        return false;
+    }
+}
+
+//----------------------------------------------------------------------------------------------------------------
+
 
 // think as a consecutive array and use binary search, find index of array and translate into matrix index.
 public class Solution {
