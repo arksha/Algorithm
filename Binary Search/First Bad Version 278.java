f143de6bf0c435f27f5812852a326fbc221b17b5
Gong <tingg@usc.edu>
2016-08-10 09:54:41 -0700

    Create First Bad Version 278.java

---
+++ Binary Search/First Bad Version 278.java
@@ -0,0 +1,37 @@
+278. First Bad Version 
+Difficulty: Easy
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+-----------------------------------------------------------------------------------------------------------------------
+typical binary search
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        if(n == 0){
+            return -1;
+        }
+        int low = 0;
+        int high = n;
+        while(low + 1 < high){
+            int mid = low + (high - low) / 2;
+            if(!isBadVersion(mid)){
+                low = mid;
+            }else{
+                high = mid;
+            }
+        }
+        if(isBadVersion(low)){
+            return low;
+        }
+        if(isBadVersion(high)){
+            return high;
+        }
+        return -1;
+    }
+}
