0b0b23771da3860ad1723f048932c1c8db925a1d
Gong <tingg@usc.edu>
2016-08-12 14:26:25 -0700

    Update Minimum Path Sum 64.java

--- DP/Minimum Path Sum 64.java
+++ DP/Minimum Path Sum 64.java
@@ -1,4 +1,9 @@
 64. Minimum Path Sum
+Difficulty: Medium
+Given a m x n grid filled with non-negative numbers, 
+find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
 
 //O(n^2) space
 public class Solution {
