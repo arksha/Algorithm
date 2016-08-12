8bdffe3f88261cdea7f98b2ae3438c921b3f3d1c
Gong <tingg@usc.edu>
2016-08-12 14:25:21 -0700

    Update and rename 64. Minimum Path Sum.java to DP/Minimum Path Sum 64.java

--- DP/Minimum Path Sum 64.java
+++ DP/Minimum Path Sum 64.java
@@ -47,3 +47,41 @@ public class Solution {
         return grid[m-1][n-1];
     }
 }
+
+
+--------------------------------------------
+update:
+
+public class Solution {
+    /**
+     * @param grid: a list of lists of integers.
+     * @return: An integer, minimizes the sum of all numbers along its path
+     */
+    public int minPathSum(int[][] grid) {
+        // write your code here
+        if(grid == null || grid.length == 0){
+            return 0;
+        }
+        if(grid[0] == null || grid[0].length == 0){
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        
+        dp[0][0] = grid[0][0];
+        
+        for(int i = 1;i<m;i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int i = 1;i<n;i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        for(int i = 1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j - 1]) + grid[i][j];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
