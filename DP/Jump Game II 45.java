9faced42d329884cd2925b71b6aad344505100ac
Gong <tingg@usc.edu>
2016-08-12 20:01:04 -0700

    fixed typo

--- DP/Jump Game II 45.java
+++ DP/Jump Game II 45.java
@@ -43,7 +43,7 @@ public class Solution {
         for(int i = 1;i<n;i++){
             dp[i] = Integer.MAX_VALUE;
             for(int j = 0;j < i;j++){
-                if(dp[i] != Integer.MAX_VALUE && j + A[j] >= i){
+                if(dp[j] != Integer.MAX_VALUE && j + A[j] >= i){
                     dp[i] = dp[j] + 1;
                     break;
                 }
