9a77b8cf5aca0ee522a2416c28fa89d9b3f31c5a
Gong <tingg@usc.edu>
2016-08-27 12:26:53 -0700

    Update and rename 263. Ugly Number I [264]II[313]Super.java to Ugly Number I II Super 263[264][313].java

--- Ugly Number I II Super 263[264][313].java
+++ Ugly Number I II Super 263[264][313].java
@@ -34,7 +34,19 @@ public class Solution {
 // 2.An ugly number must be multiplied by either 2, 3, or 5 from a smaller ugly number.
 // 3.The key is how to maintain the order of the ugly numbers. Try a similar approach of merging from three sorted lists: L1, L2, and L3.
 // 4.Assume you have Uk, the kth ugly number. Then Uk+1 must be Min(L1 * 2, L2 * 3, L3 * 5).
+---------------------------------------------------------------------------------------------------------------------
 
+Here is a time efficient solution with O(n) extra space. The ugly-number sequence is 1, 2, 3, 4, 5, 6, 8, 9, 10, 12, 15, …
+     because every number can only be divided by 2, 3, 5, 
+     one way to look at the sequence is to split the sequence to three groups as below:
+     (1) 1×2, 2×2, 3×2, 4×2, 5×2, …
+     (2) 1×3, 2×3, 3×3, 4×3, 5×3, …
+     (3) 1×5, 2×5, 3×5, 4×5, 5×5, …
+
+     We can find that every subsequence is the ugly-sequence itself (1, 2, 3, 4, 5, …) multiply 2, 3, 5. 
+     Then we use similar merge method as merge sort, to get every ugly number from the three subsequence. 
+     Every step we choose the smallest one, and move one step after.
+     
 public class Solution {
     public int nthUglyNumber(int n) {
         int[] dp = new int[n];//record ith ugly number
@@ -51,6 +63,48 @@ public class Solution {
         return dp[n-1];
     }
 }
+
+Update: use hash and heap O(nlogn)
+
+class Solution {
+    /**
+     * @param n an integer
+     * @return the nth prime number as description.
+     * use hashset to avoid dup
+     * heap store every ugly number
+     * iterate 1 to n to find n ugly number, use former ugly number multiply
+     * primes to get new ugly number and put into heap and hashset
+     * 
+     * NB: avoid large n exeed, use Long
+     */
+    public int nthUglyNumber(int n) {
+        if(n == 0){
+            return 0;
+        }
+        PriorityQueue<Long> minheap = new PriorityQueue<Long>();
+        HashSet<Long> set = new HashSet<Long>();
+        Long[] primes = new Long[3];
+        primes[0] = Long.valueOf(2);
+        primes[1] = Long.valueOf(3);
+        primes[2] = Long.valueOf(5);
+        for(int i = 0; i < 3; i++){
+            minheap.offer(primes[i]);
+            set.add(primes[i]);
+        }
+            
+        Long num = Long.valueOf(1);
+        for(int i = 1; i < n; i++){
+            num = minheap.poll();
+            for(int j = 0; j < 3; j++){
+                if(!set.contains(num * primes[j])){
+                    minheap.offer(num * primes[j]);
+                    set.add(num * primes[j]);
+                }
+            }
+        }
+        return num.intValue();
+    }
+};
 
 // 313. Super Ugly Number
 
