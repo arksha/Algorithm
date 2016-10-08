a8e8226e891b6cd13732547535b29a0d548abaff
Gong <tingg@usc.edu>
2016-10-08 15:57:31 -0700

    Update 5. Longest Palindromic Substring.java

--- 5. Longest Palindromic Substring.java
+++ 5. Longest Palindromic Substring.java
@@ -32,3 +32,35 @@ public class Solution {
         return true;
     }
 }
+-------------------------------------------------------------------------------------
+Time O(n^2) space O(1)
+
+public class Solution {
+    public String longestPalindrome(String s) {
+        if(s == null || s.length() == 0){
+            return null;
+        }
+        if(s.length() == 1){
+            return s;
+        }
+        String result = s.substring(0,1);
+        for(int i = 0; i < s.length(); i++){
+            String cur = findPalindrome(s, i, i);// center with i palindrome O(n)
+            if(cur.length() > result.length()){
+                result = cur;
+            }
+            cur = findPalindrome(s, i, i + 1);
+            if(cur.length() > result.length()){
+                result = cur;
+            }
+        }
+        return result;
+    }
+    private String findPalindrome(String s, int i, int j){// two pointers i go left ,j go right
+        while(i >= 0 && j < s.length() && s.charAt(i) == s.charAt(j)){
+            i--;
+            j++;
+        }
+        return s.substring(i + 1, j);
+    }
+}
